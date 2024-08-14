27dd1ea40ce2177bd1e08237d9c300c23a866449
Lucas Dale <lucasdale99@gmail.com>
2024-08-14 18:10:48 -0400

    P-2: barebones project again

--- app/review/page.tsx
+++ app/review/page.tsx
@@ -1,8 +1,10 @@
 import { createClient } from '@/utils/supabase/server';
 
+//TODO Add list of approved feedback from users
+
 export default async function Notes() {
   const supabase = createClient();
-  const { data: notes } = await supabase.from("notes").select();
+  const { data: notes } = (await supabase.from("notes").select());
 
   return <pre>{JSON.stringify(notes, null, 2)}</pre>
 }
