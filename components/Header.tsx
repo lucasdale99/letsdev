c9e234673e7e58c245ccc8b3873d4d8dec3b6313
Lucas Dale <lucasdale99@gmail.com>
2024-09-29 00:03:06 -0400

    theme toggle and header change

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,9 +4,9 @@ import { ThemeToggle } from "./ThemeToggle";
 export default function Header() {
   return (
     <nav className="w-full flex justify-end items-center border-b border-b-foreground/10 h-16 p-5 gap-8">
-      <ol className="flex gap-8">
+      <ol className="w-full flex justify-between">
         <li>
-          <Link href="/">Home</Link>
+          <Link href="/">Let's Dev</Link>
         </li>
         <li>
           <Link href="/blog">Blog</Link>
