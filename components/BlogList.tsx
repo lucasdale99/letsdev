5442dbcaaa651e21a992f838b097f71140f9dc43
Lucas Dale <lucasdale99@gmail.com>
2024-11-07 23:27:58 -0500

    Update method and headers

--- components/BlogList.tsx
+++ components/BlogList.tsx
@@ -4,13 +4,18 @@ import { Suspense } from "react";
 import BlogListSkeleton from "./BlogListSkeleton";
 
 export default async function BlogList() {
-  const data = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/blog`);
+  const data = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/blog`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
   const blogs = await data.json();
 
   return (
     <Suspense fallback={<BlogListSkeleton />}>
       <div className="flex flex-col">
-        {blogs.data.map((blog: IBlogList, index: number) => (
+        {blogs.data?.map((blog: IBlogList, index: number) => (
           <BlogCard
             key={blog.id}
             title={blog.title}
